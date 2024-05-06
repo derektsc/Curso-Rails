7053103653c0eef60fb0cdadd4ff91ea50adf915
Derek Barragão <derekbarragao@gmail.com>
2024-05-06 20:29:45 -0300

    create projeto_1

--- workspace/modulo_02/02_IO.rb
+++ workspace/modulo_02/02_IO.rb
@@ -5,3 +5,9 @@ puts "O seu nome é: #{nome}"
 puts "=========================="
 
 puts nome.inspect
+
+
+puts "digite seu salario: "
+sal = gets.chomp.to_f
+
+puts "seu salario atualizado é: #{sal * 1.10}"
