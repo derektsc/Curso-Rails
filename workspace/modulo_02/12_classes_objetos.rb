56c3f1f2831c6d0a3ccb4f264d84d10f766e8af4
Derek Barragão <derekbarragao@gmail.com>
2024-05-29 23:52:21 -0300

    subindo arquivo 12 classes_objetos.rb

---
+++ workspace/modulo_02/12_classes_objetos.rb
@@ -0,0 +1,19 @@
+#classes e objetos 
+
+#classes são as formas para gerar objetos, como se fosse uma plata de uma casa
+
+#objeto é o objeto propriamente pronto, uma casa pronta que voce pode utilizar para fazer outras casas mudando caracteristicas
+
+#classes são a maneira que temos de informar como queremos que nosso objeto funcione
+#ao criarmos uma classe podemos especificar os metodos e os atriutos que os ojetos possuirão, exemplo um carro: cor, motor, modelo, placa
+
+#os métodos são as ações.
+#os atributos são as caracteristicas
+
+#métodos do carro: andar pra frente, pra tras, trocar de marcha, acender farois
+#atributos do carro: cor, modelo, placa, aro da roda, cor do banco, etc
+
+
+#TUDO EM RUBY É OBJETO! POR ISSO PODE UTILIZAR .CLASS em qualquer canto
+
+puts 1.class #vai retornar integer
