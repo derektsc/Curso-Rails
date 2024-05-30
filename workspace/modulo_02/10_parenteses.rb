a33110fa6fa4c87621c7c9c57e80bbe42ae3ab35
Derek Barragão <derekbarragao@gmail.com>
2024-05-29 23:38:38 -0300

    subindo arquivo 10 parenteses.rb

---
+++ workspace/modulo_02/10_parenteses.rb
@@ -0,0 +1,19 @@
+# uso de parenteses é opcional, mas facilita na leitura
+
+puts ("derek")
+puts "derek"
+puts "----------"
+#constantes
+
+#usadas para representar valores não mutaveis
+#use letras maiusculas para declarar constantes
+
+NOME = "barragão"
+PI = 3.14
+
+puts NOME
+puts PI
+
+puts NOME.class
+puts PI.class
+
