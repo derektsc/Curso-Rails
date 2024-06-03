7851c6e795674b90b347b86a9fc9c52f3e5c9cfb
Derek Barragão <derekbarragao@gmail.com>
2024-06-03 00:35:57 -0300

    app crypto wallet

---
+++ workspace/modulo_03/21_modulo3_documentacao.rb
@@ -0,0 +1,8 @@
+#documentacao
+    #https://www.ruby-lang.org/pt/documentation/
+#core
+    #https://ruby-doc.org/3.2.2/
+#standard library
+    #https://ruby-doc.org/3.2.2/
+#api dock
+    #https://apidock.com
